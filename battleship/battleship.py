637b9d5a4fb5cd5950c891712131e6958c4389a8
Your Namekarslio <karslionl@gmail.com>
2020-02-23 20:42:16 +0100

    battleship not finished

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -1,40 +1,65 @@
-import random
-import math
+from random import randint
+
+from past.builtins import raw_input
 
 
-def drawBoard(board):
-    print('|' + board[1] + '|' + board[2] + '|' + board[3] + '|' + board[4] + '|' + board[5] + '|')
-    print('|' + board[6] + '|' + board[7] + '|' + board[8] + '|' + board[9] + '|' + board[10] + '|')
-    print('|' + board[11] + '|' + board[12] + '|' + board[13] + '|' + board[14] + '|' + board[15] + '|')
-    print('|' + board[16] + '|' + board[17] + '|' + board[18] + '|' + board[19] + '|' + board[20] + '|')
-    print('|' + board[21] + '|' + board[22] + '|' + board[23] + '|' + board[23] + '|' + board[25] + '|')
+def print_board(board):
+    for x in range(5):
+        board.append(["O"] * 5)
+    for row in board:
+        print(" ".join(row))
 
 
-board = ['_'] * 26
+board = []
+print_board(board)
+
+gemi = []
+print_board(gemi)
+
+print("look Battleship!")
 
 
-def ifverofhor():
-    if random.randint(0, 1) == 0:
-        return 'ver'
-    else:
-        return 'hor'
+def random_row(board):
+    return randint(0, len(board) - 1)
 
 
-def isSpaceFree(board, moves):
-    return board[moves] == ' '
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+#
+ship_row = random_row(gemi)
+ship_col = random_col(gemi)
+print(gemi)
 
 
-border_1 = [1, 2, 3, 4, 5]
-border_2 = [5, 10, 15, 20, 25]
-border_3 = [1, 6, 11, 16, 21]
-border_4 = [21, 22, 23, 24, 25]
 
-while True:
-    x = random.randint(1, 25)
-    print(x)
-    if ifverofhor() == 'ver' and
-        for i in range(3):
-            board[x] = 'X'
-            x += 1
-        drawBoard(board)
+# asking the user for a guess
+for turn in range(4):
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+
+    # if the user's right, the game ends
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratulations! You sunk my battleship!")
         break
+    else:
+        # warning if the guess is out of the board
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print("Oops, that's not even in the ocean.")
+
+        # warning if the guess was already made
+        elif board[guess_row][guess_col] == "X":
+            print("You guessed that one already.")
+
+        # if the guess is wrong, mark the point with an X and start again
+        else:
+            print("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+
+        # Print turn and board again here
+        print("Turn " + str(turn + 1) + " out of 4.")
+        print_board(board)
+
+    # if the user have made 4 tries, it's game over
+    if turn >= 3:
+        print("Game Over")
